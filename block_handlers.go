f1a502611de77b362618dabb4a4a3e44a99278bf
Chengcheng Pei <chengchengpei@outlook.com>
2024-04-24 11:31:22 -0700

    different block handlers

--- block_handlers.go
+++ block_handlers.go
@@ -8,6 +8,14 @@ import (
 // *utils.Deps contains *gorm.DB which can be used for CRUD
 // *utils.Deps also contains Logger
 func HandleBlock(blockNumber string, deps *utils.Deps) (bool, error) {
+	deps.Logger.Info("HandleBlock1", "block number", blockNumber)
+	//shouldRetry tells zrunner where to retry on errors
+	shouldRetry := false
+	return shouldRetry, nil
+}
+
+func HandleBlock2(blockNumber int, deps *utils.Deps) (bool, error) {
+	deps.Logger.Info("HandleBlock2", "block number", blockNumber)
 	//shouldRetry tells zrunner where to retry on errors
 	shouldRetry := false
 	return shouldRetry, nil
