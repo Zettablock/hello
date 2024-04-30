5e975a4e6cafb946ad5ef24fcbc4099f2d7b0128
Chengcheng Pei <chengchengpei@outlook.com>
2024-04-30 11:21:33 -0700

    test

---
+++ proj1/block_handlers.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"errors"
+	"github.com/Zettablock/zsource/utils"
+)
+
+// HandleBlock : shouldRetry tell zrunner whether to retry on errors
+// *utils.Deps contains *gorm.DB which can be used for CRUD
+// *utils.Deps also contains Logger
+func HandleBlock(blockNumber string, deps *utils.Deps) (bool, error) {
+	deps.Logger.Info("Proj1 HandleBlock1", "block number", blockNumber)
+	//shouldRetry tells zrunner where to retry on errors
+	shouldRetry := false
+	return shouldRetry, nil
+}
+
+func HandleBlock2(blockNumber int, deps *utils.Deps) (bool, error) {
+	deps.Logger.Info("Proj1 HandleBlock2", "block number", blockNumber)
+	return true, errors.New("block handler fake error to test")
+}
