8d4f74e75e802c373a2943d8f1b93757cb946e69
Chengcheng Pei <chengchengpei@outlook.com>
2024-05-01 14:10:48 -0700

    panic to test

--- proj2/block_handlers.go
+++ proj2/block_handlers.go
@@ -9,8 +9,9 @@ import (
 // *utils.Deps contains *gorm.DB which can be used for CRUD
 // *utils.Deps also contains Logger
 func HandleBlock(blockNumber string, deps *utils.Deps) (bool, error) {
-	deps.Logger.Info("HandleBlock1", "block number", blockNumber)
-	//shouldRetry tells zrunner where to retry on errors
+	deps.Logger.Info("HandleBlock1: panic", "block number", blockNumber)
+	panic("Panic to test")
+	////shouldRetry tells zrunner where to retry on errors
 	shouldRetry := false
 	return shouldRetry, nil
 }
