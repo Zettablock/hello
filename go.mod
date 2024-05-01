ff9dd9ccae4653b697213fbe562e9cbf58c752bc
Chengcheng Pei <chengchengpei@outlook.com>
2024-05-01 14:57:47 -0700

    upgrade zsource

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module example/user/hello
 go 1.21
 
 require (
-	github.com/Zettablock/zsource v0.0.0-20240430214704-eb71896a3029
+	github.com/Zettablock/zsource v0.0.0-20240501215111-583dd0332244
 	github.com/lib/pq v1.10.9
 	github.com/stretchr/testify v1.9.0
 	golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f
--- proj2/block_handlers.go
+++ proj2/block_handlers.go
@@ -9,8 +9,7 @@ import (
 // *utils.Deps contains *gorm.DB which can be used for CRUD
 // *utils.Deps also contains Logger
 func HandleBlock(blockNumber string, deps *utils.Deps) (bool, error) {
-	deps.Logger.Info("HandleBlock1: panic", "block number", blockNumber)
-	panic("Panic to test")
+	deps.Logger.Info("HandleBlock1", "block number", blockNumber, "pipeline_name", deps.Config.GetPipelineName())
 	////shouldRetry tells zrunner where to retry on errors
 	shouldRetry := false
 	return shouldRetry, nil
