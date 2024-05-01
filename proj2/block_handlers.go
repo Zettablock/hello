46f5ec801cc17fcc72a577aa1cdb48866b37bb16
Chengcheng Pei <chengchengpei@outlook.com>
2024-05-01 15:04:54 -0700

    update logging

--- proj2/block_handlers.go
+++ proj2/block_handlers.go
@@ -9,7 +9,7 @@ import (
 // *utils.Deps contains *gorm.DB which can be used for CRUD
 // *utils.Deps also contains Logger
 func HandleBlock(blockNumber string, deps *utils.Deps) (bool, error) {
-	deps.Logger.Info("HandleBlock1", "block number", blockNumber, "pipeline_name", deps.Config.GetPipelineName())
+	deps.Logger.Info("HandleBlock1", "block number", blockNumber, "pipeline_name2", deps.Config.GetPipelineName())
 	////shouldRetry tells zrunner where to retry on errors
 	shouldRetry := false
 	return shouldRetry, nil
